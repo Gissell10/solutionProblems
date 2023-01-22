a30f9f66c3db0188b287f4968dd10b98b951d16f
Gissell Bustos <gissellbustosb@gmail.com>
2023-01-22 13:16:03 +1100

    sumNegatives countPositives

--- solutions.js
+++ solutions.js
@@ -1,10 +1,14 @@
 function countPositivesSumNegatives(input) {
-  let outCero = input.filter((num) => num != 0);
-  //console.log(outCero);
-
-  let positives = outCero.filter((num) => num >= 1).length;
-
-  let negatives = outCero.filter((num) => num <= -1);
-  let sumNegatives = negatives.reduce((a, b) => a + b, 0);
-  console.log([positives, sumNegatives]);
+  if (input === null || input.length === 0) {
+    console.log([]);
+  } else {
+    let outCero = input.filter((num) => num != 0);
+    let countPositives = outCero.reduce((a, c) => (c >= 0 ? a + 1 : a), 0);
+    let negatives = outCero.filter((num) => num <= -1);
+    let sumNegatives = negatives.reduce((a, b) => a + b, 0);
+    console.log([countPositives, sumNegatives]);
+  }
 }
+countPositivesSumNegatives([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+]);
