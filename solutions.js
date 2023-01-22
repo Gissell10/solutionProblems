db9302638fb678d7673fe7f6f8962079f10fc58e
Gissell Bustos <gissellbustosb@gmail.com>
2023-01-23 10:54:43 +1100

    sum 2 smallest

--- solutions.js
+++ solutions.js
@@ -1,14 +1,21 @@
-function countPositivesSumNegatives(input) {
-  if (input === null || input.length === 0) {
-    console.log([]);
-  } else {
-    let outCero = input.filter((num) => num != 0);
-    let countPositives = outCero.reduce((a, c) => (c >= 0 ? a + 1 : a), 0);
-    let negatives = outCero.filter((num) => num <= -1);
-    let sumNegatives = negatives.reduce((a, b) => a + b, 0);
-    console.log([countPositives, sumNegatives]);
-  }
+// function countPositivesSumNegatives(input) {
+//   if (input === null || input.length === 0) {
+//     console.log([]);
+//   } else {
+//     let outCero = input.filter((num) => num != 0);
+//     let countPositives = outCero.reduce((a, c) => (c >= 0 ? a + 1 : a), 0);
+//     let negatives = outCero.filter((num) => num <= -1);
+//     let sumNegatives = negatives.reduce((a, b) => a + b, 0);
+//     console.log([countPositives, sumNegatives]);
+//   }
+// }
+// countPositivesSumNegatives([
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+// ]);
+function sumTwoSmallestNumbers(numbers) {
+  let organized = numbers.sort((a, b) => {
+    return a - b;
+  });
+  return organized;
 }
-countPositivesSumNegatives([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
-]);
+console.log(sumTwoSmallestNumbers([15, 28, 4.67, 2, 43]));
