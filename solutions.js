f9b6e6b6f36427e6399026a71bc0db848119650a
Gissell Bustos <gissellbustosb@gmail.com>
2023-02-28 15:00:13 +1100

    armstrong

--- solutions.js
+++ solutions.js
@@ -58,3 +58,51 @@ console.log(sorted([-5, 4, 6, 2, 10], 20));
 //   return [...numbers].sort((a, b) => a - b);
 // }
 // console.log(sorted([2, 9, 6, 0, 3, 1]));
+
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return n;
+//   }
+//   let arr = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     console.log();a
+//   }
+// }
+// //console.log(fibonacci(1));
+// console.log(fibonacci(0));
+
+function armstrong(n) {
+  let string = n.toString();
+  let pow = string.length;
+  let arr = [...string];
+  let arrNumber = arr.map((e) => parseInt(...e));
+  let potencia = arrNumber.map((e) => Math.pow(e, pow)).reduce((a, b) => a + b);
+
+  return n === potencia ? true : false;
+}
+
+console.log(armstrong(153));
+
+// armstrong([(21, 4), Programming challenge description:
+//   Alice has an even number of N beads, and each bead has a number from 1 to N painted on it. She would like to make a necklace out of all the beads, with a special requirement: any two beads next to each other on the necklace must sum to a prime number. Alice needs your help to calculate how many ways it is possible to do so.
+
+//   Example: N = 4
+
+//   There are two possible ways to build the necklace. Note that the last bead connects to the first bead.
+
+//   1 2 3 4
+//   1 4 3 2
+
+//   So the answer is 2.
+//   Input:
+//   The inputs consists of one even integer on a line. Each integer N is 2 <= N <= 18.
+//   Output:
+//   Print a line containing the number of ways to make a necklace according to the above rules.
+//   Test 1
+//   Test Input
+//   Download Test 1 Input
+//   4
+//   Expected Output
+//   Download Test 1 Output
+//   2(1, -6)]);
